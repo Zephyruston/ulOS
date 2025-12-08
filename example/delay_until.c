d2446e574e8d141ea0a2e1cd2cbe136ad7588b97
zhuqinsheng <2499311743@qq.com>
2025-12-08 20:20:34 +0800

    添加部分example文件

---
+++ example/delay_until.c
@@ -0,0 +1,67 @@
+#include "ulOS_thread.h"
+
+static ul_thread_t *thread1;
+static ul_thread_t *thread2;
+
+static void thread1_entry(void *parameter)
+{ 
+    volatile ul_uint32_t i = 0;
+    uint8_t j = 0;
+    
+    while(j < 10)
+    {
+        printf("[1]delay_thread running...ulOS tick:%d\r\n", (int)ulOS_get_tick());
+        
+        for(i = 0; i < 100000; i++);    // 模拟运行耗时
+        
+        ul_thread_delay(500);  
+        
+        j++;
+    }
+    
+    printf("[1]delay_thread delete...\r\n");
+    return;
+}
+
+static void thread2_entry(void *parameter)
+{
+    volatile ul_uint32_t i = 0;
+    uint8_t j = 0;
+    
+    ul_tick_t last_wake_time = ulOS_get_tick();
+    
+    while(j < 10)
+    {
+        printf("[2]delay_until_thread running...ulOS tick:%d\r\n", (int)ulOS_get_tick());
+        
+        for(i = 0; i < 100000; i++);    // 模拟运行耗时
+        
+        ul_thread_delay_until(&last_wake_time, 500);
+        
+        j++;
+    }   
+    
+    printf("[2]delay_until_thread delete...\r\n");
+    return;
+}
+
+/* 测试函数 */
+void example_delay_until(void)
+{
+    thread1 = ul_thread_create("du_t1",
+                               thread1_entry,
+                               NULL,
+                               512,
+                               1,
+                               1);
+    ul_thread_startup(thread1);
+    
+    thread2 = ul_thread_create("du_t2",
+                               thread2_entry,
+                               NULL,
+                               512,
+                               0,
+                               1);
+    ul_thread_startup(thread2);
+    
+}
---
+++ example/dynamic_memory.c
@@ -0,0 +1,47 @@
+#include "ulOS_thread.h"
+
+static ul_thread_t *thread1;
+
+static void thread1_entry(void *parameter)
+{ 
+    int i;
+    char *ptr = UL_NULL; /* 内存块的指针 */
+    
+    printf("ulOS heap total size :%d byte\r\n", ul_heap_get_total_size());
+    
+    for (i = 0; ; i++)
+    {
+        /* 每次分配 (1 << i) 大小字节数的内存空间 */
+        ptr = ul_malloc(1 << i);
+
+        /* 如果分配成功 */
+        if (ptr != UL_NULL)
+        {
+            printf("get memory :%d byte\r\n", (1 << i));
+            /* 释放内存块 */
+            ul_free(ptr);
+            printf("free memory :%d byte\r\n", (1 << i));
+            ptr = UL_NULL;
+        }
+        else
+        {
+            printf("try to get %d byte memory failed!\r\n", (1 << i));
+            printf("ulOS heap free size :%d byte\r\n", ul_heap_get_free_size());
+            return;
+        }
+    }
+
+    return;
+}
+
+/* 测试函数 */
+void example_dynamic_memmory(void)
+{
+    thread1 = ul_thread_create("ul_malloc_t1",
+                               thread1_entry,
+                               NULL,
+                               512,
+                               1,
+                               1);
+    ul_thread_startup(thread1);
+}
