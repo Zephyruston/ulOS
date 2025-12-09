6efd78ca00579c1d86139b576dbbec9462959dba
zhuqinsheng <2499311743@qq.com>
2025-12-09 15:14:22 +0800

    组件：按键驱动优化

--- component/inc/ul_button.h
+++ component/inc/ul_button.h
@@ -3,85 +3,107 @@
  * Date           Author       Notes
  * 2025-7-21     zhuqinsheng   the first version
  * 2025-8-13     zhuqinsheng   修改了ul_button_t的一些属性, 主要增加了type成员
+ * 2025-12-9     zhuqinsheng   优化了初始化机制,新增长按循环触发功能
  */
-#ifndef __BUTTON_H_
-#define __BUTTON_H_
+#ifndef __UL_BUTTON_H__
+#define __UL_BUTTON_H__
 
 #include "ul_config.h"
-#include "ul_list.h"
 
 #ifdef __cplusplus
 extern "C" {
-#endif  /* __cplusplus */
+#endif /* __cplusplus */
 
-/*
- * button type definition
- */
-#define UL_BUTTON_TYPE_STATIC      0x01
-#define UL_BUTTON_TYPE_DYNAMIC     0x02
-
-/*
- * button event enumeration
- */
+/* 按键事件枚举 */
 typedef enum
 {
-    BUTTON_EVENT_PRESSED        = 0x01,      /* button单击 */
-    BUTTON_EVENT_LONGPRESS      = 0x02,      /* button长按 */
-    BUTTON_EVENT_DOUBLE_CLICK   = 0x03,      /* button双击 */
+    UL_BUTTON_EVENT_PRESSED        = 0x01,    /* 单击事件 */
+    UL_BUTTON_EVENT_LONGPRESS      = 0x02,    /* 长按事件 */
+    UL_BUTTON_EVENT_DOUBLE_CLICK   = 0x03,    /* 双击事件 */
 } button_event_e;
 
-/*
- * button structure
- */
+/* 前向声明 */
 typedef struct ul_button ul_button_t;
-typedef void (*button_event_callback_t)(ul_button_t *self,  button_event_e event);
-struct ul_button 
-{
-	ul_uint32_t             id;
-    ul_uint8_t              stage;
-    ul_uint32_t             long_ticks_count;
-    ul_uint32_t             double_ticks_count;
-    
-    struct 
-    {
-        ul_uint32_t         dp_th; /* 双击时间阈值ms */
-        ul_uint32_t         lp_th; /* 长按时间阈值ms */
-    }config;
-    
-    button_event_callback_t event_callback;
-    button_event_e          event;
-    ul_uint8_t              type;
-    ul_list_t               node;
-    
-    /* 移植接口 */
-    /**
-      * @brief 读取button状态
-      * @attention 用户实现此接口时其返回值必须为按下时为1,松开时为0
-      * @return 0 按钮未被按下
-      * @return 1 按钮已被按下
-    */
-    ul_uint32_t (*read_pin)(ul_button_t *self);
 
-};
+/* 回调函数类型定义 */
+typedef void (*ul_button_event_cb_func)(button_event_e event);
 
-/*
- * button interface
+/* 移植接口 */
+/**
+ * @brief 读取button状态
+ * @param self 按键对象指针
+ * @return 0 按钮未被按下
+ * @return 1 按钮已被按下
+ * @note 用户实现此接口时其返回值必须为按下时为1,松开时为0
  */
-ul_ecode ul_button_object_init(ul_button_t *self);
-ul_ecode ul_button_object_detach(ul_button_t *self);
-ul_button_t* ul_button_object_create(void);
-ul_ecode ul_button_object_delete(ul_button_t *self);
+typedef ul_uint32_t (*ul_button_read_fuc)(void);
 
-void ul_button_presstime_config(ul_button_t *self, ul_uint32_t longpress_time, ul_uint32_t doublepress_time);
-void ul_button_register_callback(ul_button_t *self, button_event_callback_t func);
+/* 默认时间配置 */
+#define UL_BUTTON_CONFIG_LP_TIME    (800)    /* 长按时间阈值(ms) */
+#define UL_BUTTON_CONFIG_DP_TIME  (300)    /* 双击时间阈值(ms) */
 
-void ul_button_defalut_event_callback(ul_button_t *self, button_event_e event);
+/* 按键接口函数声明 */
+/**
+ * @brief 初始化一个静态的button对象
+ * @param self button对象的地址
+ * @param read_pin 读取按键状态的函数指针
+ * @param cb_func 按键事件回调函数指针
+ * @return 错误代码
+ */
+ul_ecode ul_button_init(ul_button_t *self,
+                               ul_button_read_fuc read_pin,
+                               ul_button_event_cb_func cb_func);
 
+/**
+ * @brief 从系统中分离button对象
+ * @param self button对象的地址
+ * @return 错误代码
+ */
+ul_ecode ul_button_detach(ul_button_t *self);
+
+/**
+ * @brief 动态创建一个button对象
+ * @param read_pin 读取按键状态的函数指针
+ * @param cb_func 按键事件回调函数指针
+ * @return button对象的地址
+ */
+ul_button_t* ul_button_create(ul_button_read_fuc read_pin,
+                                     ul_button_event_cb_func cb_func);
+
+/**
+ * @brief 删除一个动态的button对象
+ * @param self button对象的地址
+ * @return 错误代码
+ */
+ul_ecode ul_button_delete(ul_button_t *self);
+
+/**
+ * @brief 设置button长按和双击的时间阈值
+ * @param self button对象的地址
+ * @param lp_time 长按时间阈值(ms)
+ * @param dp_time 双击时间阈值(ms)
+ */
+void ul_button_presstime_change(ul_button_t *self, 
+                               ul_uint32_t lp_time, 
+                               ul_uint32_t dp_time);
+
+/**
+ * @brief 注册button事件回调函数
+ * @param self button对象的地址
+ * @param func 要注册的事件回调函数地址
+ */
+void ul_button_register_callback(ul_button_t *self, 
+                                ul_button_event_cb_func func);
+
+/**
+ * @brief button定时器回调函数
+ * @param period 调用周期(ms)
+ * @note 需以一个固定周期调用此函数
+ */
 void ul_button_timer_callback(ul_uint32_t period);
 
 #ifdef __cplusplus
 }
-#endif  /* __cplusplus */
+#endif /* __cplusplus */
 
-#endif
-
+#endif /* __UL_BUTTON_H__ */
--- component/src/ul_button.c
+++ component/src/ul_button.c
@@ -3,18 +3,51 @@
  * Date           Author       Notes
  * 2025-7-21     zhuqinsheng   the first version
  * 2025-8-13     zhuqinsheng   修改了ul_button_t的一些属性, 主要增加了type成员
+ * 2025-12-9     zhuqinsheng   优化了初始化机制,新增长按循环触发功能
  */
-
 #include "ul_button.h"
+#include "ul_object.h"
+
+struct ul_button
+{
+    ul_uint32_t             id;
+    ul_uint8_t              stage;
+    ul_uint32_t             long_ticks_count;
+    ul_uint32_t             double_ticks_count;
+
+    struct
+    {
+        ul_uint32_t         dp_th; /* 双击时间阈值ms */
+        ul_uint32_t         lp_th; /* 长按时间阈值ms */
+    } config;
+
+    ul_button_event_cb_func event_callback;
+    button_event_e          event;
+    ul_uint8_t              type;
+    ul_list_t               node;
+
+    /* 移植接口 */
+    /**
+      * @brief 读取button状态
+      * @attention 用户实现此接口时其返回值必须为按下时为1,松开时为0
+      * @return 0 按钮未被按下
+      * @return 1 按钮已被按下
+    */
+    ul_button_read_fuc read_pin;
+};
+
+/* 按键类型定义 */
+#define UL_BUTTON_TYPE_STATIC      0x01    /* 静态创建的按键 */
+#define UL_BUTTON_TYPE_DYNAMIC     0x02    /* 动态创建的按键 */
 
 #define BUTTON_STAGE_NORMAL         0
 #define BUTTON_STAGE_SHAKE          1
 #define BUTTON_STAGE_LONGPRESS      2
 #define BUTTON_STAGE_DOUBLEPRESS    3
 #define BUTTON_STAGE_IDLE           4
+#define BUTTON_STAGE_REPEAT_PRESS   5
 
-static ul_list_t *button_list_head_node;
-static ul_uint8_t ul_list_head_node_created_flag = 0;
+static ul_list_t button_list_head_node = UL_LIST_HEAD_INIT(button_list_head_node);
 
 static void ul_button_task_handler(ul_button_t * self, ul_uint32_t period)
 {
@@ -22,113 +55,115 @@ static void ul_button_task_handler(ul_button_t * self, ul_uint32_t period)
     {
         return;
     }
-    ul_uint32_t pin = self->read_pin(self);
+
+    ul_uint32_t pin = self->read_pin();
 
     switch (self->stage)
     {
-        case BUTTON_STAGE_NORMAL:
-        {
-            if (pin == 1)
-            {
-                self->stage = BUTTON_STAGE_SHAKE;
-            }
-        }
-        break;
-
-        case BUTTON_STAGE_SHAKE:
-        {
-            if (pin == 1)
-            {
-                self->stage = BUTTON_STAGE_LONGPRESS;
-            }
-            else
-            {
-                self->stage = BUTTON_STAGE_NORMAL;
-            }
-        }
-        break;
-
-        case BUTTON_STAGE_LONGPRESS:
-        {
-            if (pin == 0)
-            {
-                self->stage = BUTTON_STAGE_DOUBLEPRESS;
-            }
-            else
-            {
-                self->long_ticks_count += period;
-
-                if (self->long_ticks_count >= self->config.lp_th)
-                {
-                    self->event_callback(self, BUTTON_EVENT_LONGPRESS);
-                    self->stage = BUTTON_STAGE_IDLE;
-                }
-            }
-        }
-        break;
-
-        case BUTTON_STAGE_DOUBLEPRESS:
-        {
-            self->double_ticks_count += period;
-
-            if (pin == 1)
-            {
-                self->event_callback(self, BUTTON_EVENT_DOUBLE_CLICK);
-                self->stage = BUTTON_STAGE_IDLE;
-            }
-            else if (self->double_ticks_count > self->config.dp_th)
-            {
-                self->event_callback(self, BUTTON_EVENT_PRESSED);
-                self->stage = BUTTON_STAGE_IDLE;
-            }
-        }
-        break;
-
-        case BUTTON_STAGE_IDLE:
-        {
-            if (pin == 0)
-            {
-                self->double_ticks_count = 0;
-                self->long_ticks_count = 0;
-                self->stage = BUTTON_STAGE_NORMAL;
-            }
-        }
-        break;
-    }
-}
-
-static void ul_list_head_node_create(ul_list_t **head)
-{
-    /* 如果没有pin设备的头节点，创建一个头节点 */
-    if (ul_list_head_node_created_flag == 0)
+    case BUTTON_STAGE_NORMAL:
     {
-        *head = (ul_list_t *)ul_malloc(sizeof(ul_list_t));
-
-        if (*head != NULL)
+        if (pin == 1)
         {
-            ul_list_init(*head);
+            self->stage = BUTTON_STAGE_SHAKE;
+        }
+    }
+    break;
 
-            if (ul_list_isempty(*head))
+    case BUTTON_STAGE_SHAKE:
+    {
+        if (pin == 1)
+        {
+            self->stage = BUTTON_STAGE_LONGPRESS;
+            self->long_ticks_count = 0;  // 重置长按计数
+        }
+        else
+        {
+            self->stage = BUTTON_STAGE_NORMAL;
+        }
+    }
+    break;
+
+    case BUTTON_STAGE_LONGPRESS:
+    {
+        if (pin == 0)
+        {
+            self->stage = BUTTON_STAGE_DOUBLEPRESS;
+        }
+        else
+        {
+            self->long_ticks_count += period;
+
+            if (self->long_ticks_count >= self->config.lp_th)
             {
-                ul_list_head_node_created_flag = 1;
+                self->event_callback(UL_BUTTON_EVENT_LONGPRESS);
+                self->stage = BUTTON_STAGE_REPEAT_PRESS;  // 进入连续长按状态
+                self->long_ticks_count = 0;  // 重置计数器
             }
         }
     }
+    break;
+
+    case BUTTON_STAGE_REPEAT_PRESS:  // 新增的连续长按状态处理
+    {
+        if (pin == 0)
+        {
+            self->stage = BUTTON_STAGE_NORMAL;
+        }
+        else
+        {
+            self->long_ticks_count += period;
+
+            if (self->long_ticks_count >= self->config.lp_th)
+            {
+                self->event_callback(UL_BUTTON_EVENT_LONGPRESS);
+                self->long_ticks_count = 0;  // 重置计数器，准备下一次连续长按
+            }
+        }
+    }
+    break;
+
+    case BUTTON_STAGE_DOUBLEPRESS:
+    {
+        self->double_ticks_count += period;
+
+        if (pin == 1)
+        {
+            self->event_callback(UL_BUTTON_EVENT_DOUBLE_CLICK);
+            self->stage = BUTTON_STAGE_IDLE;
+        }
+        else if (self->double_ticks_count > self->config.dp_th)
+        {
+            self->event_callback(UL_BUTTON_EVENT_PRESSED);
+            self->stage = BUTTON_STAGE_IDLE;
+        }
+    }
+    break;
+
+    case BUTTON_STAGE_IDLE:
+    {
+        if (pin == 0)
+        {
+            self->double_ticks_count = 0;
+            self->long_ticks_count = 0;
+            self->stage = BUTTON_STAGE_NORMAL;
+        }
+    }
+    break;
+    }
 }
-
-
 /**
  * 注册button事件回调函数
  *
  * @param self button对象的地址
- * @param button_event_callback_t 要注册的事件回调函数地址
+ * @param ul_button_event_cb_func 要注册的事件回调函数地址
  */
-void ul_button_register_callback(ul_button_t * self, button_event_callback_t func)
+void ul_button_register_callback(ul_button_t * self, ul_button_event_cb_func func)
 {
     if (func == NULL)
     {
         return;
     }
+
     self->event_callback = func;
 }
 
@@ -138,9 +173,9 @@ void ul_button_register_callback(ul_button_t * self, button_event_callback_t fun
  * @param self button对象的地址
  * @param event 事件代码
  */
-ul_weak void ul_button_defalut_event_callback(ul_button_t * self, button_event_e event)
+ul_weak void ul_button_defalut_event_callback(button_event_e event)
 {
-    
+
 }
 
 /**
@@ -150,21 +185,33 @@ ul_weak void ul_button_defalut_event_callback(ul_button_t * self, button_event_e
  *
  * @return 错误代码
  */
-ul_ecode ul_button_object_init(ul_button_t * self)
+ul_ecode ul_button_init(ul_button_t *self,
+                               ul_button_read_fuc read_pin,
+                               ul_button_event_cb_func cb_func)
 {
-    if (self == NULL)
+    if (self == UL_NULL || read_pin == UL_NULL)
     {
         return UL_ERROR;
     }
-    /* 第一次创建头节点 */
-    ul_list_head_node_create(&button_list_head_node);
-    
+
+    self->read_pin = read_pin;
     self->long_ticks_count      = 0;
     self->double_ticks_count    = 0;
-    self->event_callback        = ul_button_defalut_event_callback;
+    self->config.lp_th = UL_BUTTON_CONFIG_LP_TIME;
+    self->config.dp_th = UL_BUTTON_CONFIG_DP_TIME;
+
+    if (cb_func == UL_NULL)
+    {
+        self->event_callback    = ul_button_defalut_event_callback;
+    }
+    else
+    {
+        self->event_callback    = cb_func;
+    }
+
     /* 对象节点插入链表最后 */
-    ul_list_insert_before(button_list_head_node, &self->node);   // 节点插入到链表最后一个
-    
+    ul_list_insert_before(&button_list_head_node, &self->node);   // 节点插入到链表最后一个
+
     self->type                  |= UL_BUTTON_TYPE_STATIC;
     self->stage = BUTTON_STAGE_NORMAL;
     return UL_EOK;
@@ -175,29 +222,45 @@ ul_ecode ul_button_object_init(ul_button_t * self)
  *
  * @return button对象的地址
  */
-ul_button_t* ul_button_object_create(void)
+ul_button_t *ul_button_create(ul_button_read_fuc read_pin,
+                                     ul_button_event_cb_func cb_func)
 {
-    ul_button_t * button_object;
-    /* 第一次创建头节点 */
-    ul_list_head_node_create(&button_list_head_node);
-        
-    button_object = (ul_button_t *)ul_malloc(sizeof(struct ul_button));
-
-    if (button_object == NULL)
+    if (read_pin == UL_NULL)
     {
         return UL_NULL;
     }
-    
-    button_object->long_ticks_count     = 0;
-    button_object->double_ticks_count   = 0;
-    button_object->stage                = BUTTON_STAGE_NORMAL;
-    button_object->event_callback       = ul_button_defalut_event_callback;
-    button_object->type                |= UL_BUTTON_TYPE_DYNAMIC;
-    /* 对象节点插入链表最后 */
-    ul_list_insert_before(button_list_head_node, &button_object->node);
 
-    
-    return button_object;
+    ul_button_t *self;
+
+    self = (ul_button_t *)ul_malloc(sizeof(struct ul_button));
+
+    if (self == NULL)
+    {
+        return UL_NULL;
+    }
+
+    self->read_pin = read_pin;
+    self->long_ticks_count      = 0;
+    self->double_ticks_count    = 0;
+    self->config.lp_th = UL_BUTTON_CONFIG_LP_TIME;
+    self->config.dp_th = UL_BUTTON_CONFIG_DP_TIME;
+    self->stage                = BUTTON_STAGE_NORMAL;
+
+    if (cb_func == UL_NULL)
+    {
+        self->event_callback        = ul_button_defalut_event_callback;
+    }
+    else
+    {
+        self->event_callback        = cb_func;
+    }
+
+    self->type                |= UL_BUTTON_TYPE_DYNAMIC;
+    /* 对象节点插入链表最后 */
+    ul_list_insert_before(&button_list_head_node, &self->node);
+
+
+    return self;
 }
 
 /**
@@ -205,21 +268,22 @@ ul_button_t* ul_button_object_create(void)
  *
  * @return 错误代码
  */
-ul_ecode ul_button_object_delete(ul_button_t * self)
+ul_ecode ul_button_delete(ul_button_t * self)
 {
     if (self == NULL)
     {
         return UL_ERROR;
     }
+
     /* 从button链表移除 */
     ul_list_remove(&self->node);
-    
+
     /* 动态的对象需要释放内存 */
     if (self->type & UL_BUTTON_TYPE_DYNAMIC)
     {
         ul_free(self);
     }
-    
+
     return UL_EOK;
 }
 
@@ -230,10 +294,10 @@ ul_ecode ul_button_object_delete(ul_button_t * self)
  * @param longpress_time 长按时间阈值
  * @param doublepress_time 双击时间阈值
  */
-void ul_button_presstime_config(ul_button_t * self, ul_uint32_t longpress_time, ul_uint32_t doublepress_time)
+void ul_button_presstime_change(ul_button_t *self, ul_uint32_t lp_time, ul_uint32_t dp_time)
 {
-    self->config.lp_th = longpress_time;
-    self->config.dp_th = doublepress_time;
+    self->config.lp_th = lp_time;
+    self->config.dp_th = dp_time;
 }
 
 /**
@@ -243,15 +307,10 @@ void ul_button_presstime_config(ul_button_t * self, ul_uint32_t longpress_time, 
  */
 void ul_button_timer_callback(ul_uint32_t period)
 {
-    ul_list_t * now;
+    ul_list_t *now;
 
-    /* 头节点未创建 */
-    if (button_list_head_node == NULL)
-    {
-        return;
-    }
     /* 遍历button链表 */
-    ul_list_for_each(now, button_list_head_node)
+    ul_list_for_each(now, &button_list_head_node)
     {
         ul_button_task_handler(ul_list_entry(now, struct ul_button, node), period);
     }
